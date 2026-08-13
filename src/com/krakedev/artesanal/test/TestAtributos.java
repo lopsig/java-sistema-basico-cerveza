ac5aba7db411ccbc2bf81bcebc3f218c89c6b239
lopsig <jonathan.ls.trabajo@gmail.com>
2026-08-12 21:30:24 -0500

    feat: add TestAtributos class

---
+++ src/com/krakedev/artesanal/test/TestAtributos.java
@@ -0,0 +1,16 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.Maquina;
+
+public class TestAtributos {
+    public static void main(String[] args) {
+        //INSTANCIAS
+        Maquina rubia = new Maquina("Pilsener", "La cerveza del pueblo", 0.02, 9000);
+
+        rubia.imprimir();
+
+        rubia.setNombreCerveza("Golden Ale");
+        rubia.setDescripcionCerveza("Cerveza Artesanal");
+        rubia.imprimir();
+    }
+}
